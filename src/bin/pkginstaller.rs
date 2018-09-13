36f6649301ef4638bf662706d1c5deb488bd1e97
WebD <eliangu3600103@gmail.com>
2018-09-13 08:47:00 -0400

    feat(cli): Add -f option

--- src/bin/pkginstaller.rs
+++ src/bin/pkginstaller.rs
@@ -1,6 +1,7 @@
 extern crate clap;
 extern crate pkginstaller;
 use clap::{App, Arg, ArgMatches};
+use std::fs::File;
 use pkginstaller::logger::{LogType, log};
 
 fn build_cli() -> App<'static, 'static> {
@@ -14,6 +15,12 @@ fn build_cli() -> App<'static, 'static> {
             .value_name("PKG_MANAGER")
             .help("The package manager that you want to use.")
             .takes_value(true))
+        .arg(Arg::with_name("file")
+            .short("f")
+            .value_name("FILE")
+            .help("JSON file where the pkg are listed")
+            .default_value("packages.json")
+            .takes_value(true))
 }
 
 fn main() {
@@ -21,7 +28,13 @@ fn main() {
     match args.value_of("pkgmanager") {
         Some(pkgm_name) => {
             match pkginstaller::is_valid_pkg(pkgm_name) {
-                Some(pkgm) => println!("{:?}", pkgm),
+                Some(pkgm) => {
+                   let file_path = args.value_of("file").unwrap();
+                   match File::open(file_path) {
+                       Ok(file) => println!("{:?}", file),
+                       Err(_s) => log("Unable to open the file", LogType::Error)
+                   }
+                },
                 None => log("Please provide a valid package manager.", LogType::Error)
             }
         },
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,20 @@
 extern crate colored;
 
 pub mod logger;
+use std::fs::File;
 
 #[derive(Debug)]
 pub struct PKGManager {
     name: String,
     install_command: String
 }
+
+impl PKGManager {
+    pub fn install(file: File) {
+        
+    }
+}
+
 
 fn get_pkgm() -> Vec<PKGManager> {
     vec![PKGManager{
